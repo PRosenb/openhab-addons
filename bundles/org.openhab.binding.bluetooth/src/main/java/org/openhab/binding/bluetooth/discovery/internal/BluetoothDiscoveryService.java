53e25919feab4676f483445141a345fbbeac9266
Peter Rosenberg <prosenb.dev@gmail.com>
2021-01-13 15:33:38 +0100

    [bluetooth] Notify things again after they are removed from listeners like Inbox
    Signed-off-by: Peter Rosenberg <prosenb.dev@gmail.com>

--- bundles/org.openhab.binding.bluetooth/src/main/java/org/openhab/binding/bluetooth/discovery/internal/BluetoothDiscoveryService.java
+++ bundles/org.openhab.binding.bluetooth/src/main/java/org/openhab/binding/bluetooth/discovery/internal/BluetoothDiscoveryService.java
@@ -131,6 +131,13 @@ public class BluetoothDiscoveryService extends AbstractDiscoveryService implemen
         for (BluetoothAdapter adapter : adapters) {
             adapter.scanStop();
         }
+
+        // The method `removeOlderResults()` removes the Things from listeners like `Inbox`.
+        // We therefore need to reset `latestSnapshot` so that the Things are notified again next time.
+        // Results newer than `getTimestampOfLastScan()` will also be notified again but do not lead to duplicates.
+        discoveryCaches.values().forEach(discoveryCache -> {
+            discoveryCache.latestSnapshot = null;
+        });
         removeOlderResults(getTimestampOfLastScan());
     }
 
@@ -293,6 +300,9 @@ public class BluetoothDiscoveryService extends AbstractDiscoveryService implemen
 
         private void retractDiscoveryResult(BluetoothAdapter adapter, DiscoveryResult result) {
             Set<DiscoveryResult> results = discoveryResults.remove(adapter);
+            // The method `thingRemoved()` removes the Thing from listeners like `Inbox`.
+            // We therefore need to reset `latestSnapshot` so that the Thing is notified again next time.
+            latestSnapshot = null;
             if (results != null) {
                 for (DiscoveryResult r : results) {
                     thingRemoved(r.getThingUID());
